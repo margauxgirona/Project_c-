431bffb7c1d09a7bafff40af487cfb6af06c4fab
jejerarama <jeremy.rama@edu.devinci.fr>
2025-11-12 16:28:50 +0100

    Update MT.cpp

--- Project_C++/MT.cpp
+++ Project_C++/MT.cpp
@@ -1,4 +1,16 @@
 #include "MT.h"
+#include <random>
 
-// Une seule ligne suffit !
-std::mt19937 MT::_gen;
+std::mt19937 MT::_gen{ std::random_device{}() };
+
+double MT::rand_unif() 
+{
+    static std::uniform_real_distribution<> loi_uniforme(0.0, 1.0);
+    return loi_uniforme(_gen);
+}
+
+double MT::rand_norm() 
+{
+    static std::normal_distribution<> loi_normale(0.0, 1.0);
+    return loi_normale(_gen);
+}
