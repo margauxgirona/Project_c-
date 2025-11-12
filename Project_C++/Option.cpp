aa04048d4b0d421cc91b88d9259b93422876b664
Mathis403 <mathis403@DESKTOP-J0HUB2G.localdomain>
2025-11-12 16:42:00 +0100

    Option.cpp

--- Project_C++/Option.cpp
+++ Project_C++/Option.cpp
@@ -1,4 +1,26 @@
 #include "Option.h"
+
+Option::Option(double expiry) : _expiry(expiry) {}
+
+double Option::getExpiry() const {
+		return _expiry;
+	}
+
+
+double Option::payoffPath(const std::vector<double>& spots) const
+	{
+		if (spots.empty()) {
+			return 0.0;
+		}
+		return payoff(spots.back());
+	}
+
+bool Option::isAsianOption() const { 
+	return false; 
+}
+
 bool Option:: isAmericanOption() {
 	return false;
 }
+
+Option::~Option() {}
