a97bd94074bbd988185e2a72027d67acbb55af9f
jejerarama <jeremy.rama@edu.devinci.fr>
2025-11-12 16:29:33 +0100

    Update MT.h

--- Project_C++/MT.h
+++ Project_C++/MT.h
@@ -4,17 +4,8 @@
 class MT {
 private:
     static std::mt19937 _gen;
-
-    MT() = delete; // empêche la création d'instances
-
+    MT() = delete;
 public:
-    static double rand_unif() {
-        static std::uniform_real_distribution<> dist(0.0, 1.0);
-        return dist(_gen);
-    }
-
-    static double rand_norm() {
-        static std::normal_distribution<> dist(0.0, 1.0);
-        return dist(_gen);
-    }
+    static double rand_unif();
+    static double rand_norm();
 };
