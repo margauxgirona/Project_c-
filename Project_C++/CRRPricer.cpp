2e090fabef59ac3d081ad145c2f340bc02691d41
Mathis403 <mathis403@DESKTOP-J0HUB2G.localdomain>
2025-12-05 15:24:34 +0100

    Modif CRRPricer

--- Project_C++/CRRPricer.cpp
+++ Project_C++/CRRPricer.cpp
@@ -50,6 +50,67 @@ double CRRPricer::intPow(double a, int exponent) const{
 }
 
 void CRRPricer::compute(){
+     // Precompute powers (1+u)^i and (1+d)^j
+    vector<double> upPow(_depth+1), downPow(_depth+1);
+    upPow[0] = downPow[0] = 1.0;
+
+    for (int i = 1; i <= _depth; i++) {
+        upPow[i]   = upPow[i-1]   * (1 + _u);
+        downPow[i] = downPow[i-1] * (1 + _d);
+    }
+
+    for (int i = 0; i <= _depth; i++) {
+        double S = _S0 * upPow[i] * downPow[_depth - i];
+        _H.setNode(_depth, i, _option->payoff(S));
+    }
+
+    double q = (_r - _d) / (_u - _d);
+    double inv_r = 1.0 / (1 + _r);
+
+    if (_option->isAmericanOption()) {
+
+        for (int n = _depth - 1; n >= 0; --n) {
+            for (int i = 0; i <= n; i++) {
+
+                double continuation =
+                     (q * _H.getNode(n+1, i+1) +
+                           (1 - q) * _H.getNode(n+1, i)) / inv_r;
+
+    
+                double S = _S0 * upPow[i] * downPow[n - i];
+                double intrinsic = _option->payoff(S);
+
+                bool exercise = intrinsic >= continuation;
+
+                if (intrinsic >= continuation) {
+                    _H.setNode(n, i, intrinsic);
+                    _Exercise.setNode(n, i, true);}
+                else {
+                    _H.setNode(n, i, continuation);
+                    _Exercise.setNode(n, i, false);}
+
+            }
+        }
+    }
+
+    else {
+        // compute _H
+        for (int n = _depth - 1; n >= 0; n--) {
+            for (int i = 0; i <= n; i++) {
+                double value = q * _H.getNode(n + 1, i + 1) + (1 - q) * _H.getNode(n + 1, i);
+                value /= (1 + _r);
+                _H.setNode(n, i, value);
+            }
+        }
+    }
+
+    _computed = true;
+
+
+}
+
+/*
+void CRRPricer::compute(){
     // fill the _stockTree with the prices
     //Boucler une seule fois
     for(int n = 0; n <= _depth; n++){
@@ -67,7 +128,6 @@ void CRRPricer::compute(){
     double q = (_r - _d) / (_u - _d);
 
     
- // Ajout MF
     if (_option->isAmericanOption()) {
         for (int n = _depth - 1; n >= 0; n--) {
             for (int i = 0; i <= n; i++) {
@@ -101,6 +161,8 @@ void CRRPricer::compute(){
     _computed = true; 
 }
 
+*/
+
 double CRRPricer::get(int n, int i) const {
     if (n > _depth || i > n)
         throw std::out_of_range("CRRPricer::get indices out of range");
@@ -124,6 +186,42 @@ double CRRPricer::operator()(bool closed_form) {
     if (!(_computed))
         compute();
     
+    if (!closed_form)
+        return _H.getNode(0, 0);
+
+    double discount = 1.0 / std::pow(1 + _r, _depth);
+
+    double q  = (_r - _d) / (_u - _d);
+    double pq = q;           // q^i
+    double p1 = 1.0 - q;     // (1-q)
+    double p1pow = std::pow(p1, _depth); // initial: (1-q)^N
+
+    long double C = 1.0;     // C(N,0)
+
+    double sum = 0.0;
+
+    for (int i = 0; i <= _depth; i++) {
+
+        sum += C * pq * p1pow * _option->payoff(_S.getNode(_depth, i));
+
+        if (i < _depth) { // To avoid calculate the last update
+
+            C = C * (_depth - i) / (i + 1);
+
+            // Update powers
+            pq    *= q;        // q^i   -> q^(i+1)
+            p1pow /= p1;       // (1-q)^(N-i) -> (1-q)^(N-i-1)
+        }
+    }
+
+    return discount * sum;
+}
+
+/*
+double CRRPricer::operator()(bool closed_form) {
+    if (!(_computed))
+        compute();
+    
     if (!(closed_form))
         return _H.getNode(0, 0);
     else{
@@ -137,4 +235,4 @@ double CRRPricer::operator()(bool closed_form) {
         }
         return discount * sum;
     }
-}
+}*/
